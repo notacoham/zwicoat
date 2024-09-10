5dee0ad4c13a168b1608ff83811a836293515184
Alex Cottam <alexcottam12@gmail.com>
2024-09-10 14:46:17 -0600

    add initial pages files

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Landing } from './Landing';
+export { default as HomeLayout } from './HomeLayout';
+export { default as Technology } from './Technology';
+export { default as Contact } from './Contact';
+export { default as About } from './About';
