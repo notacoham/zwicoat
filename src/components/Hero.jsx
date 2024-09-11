94a09f7ec27804e7856daf3008071c31f97118c9
Alex Cottam <alexcottam12@gmail.com>
2024-09-11 14:48:34 -0600

    set default index.css stylings and setup Hero and Navbar wrappers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/HeroWrapper';
 
 const Hero = () => {
   return (
-    <div className="hero-center">
-      <div className="hero-content">
-        <h1 className="hero-header">Hero 1</h1>
-        <h1 className="hero-subheader">Hero 2</h1>
-        <p className="hero-body">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          laboriosam nemo odit aperiam perspiciatis quasi.
-        </p>
+    <Wrapper>
+      <div className="hero-center">
+        <div className="hero-content">
+          <h1 className="hero-header">Hero 1</h1>
+          <h1 className="hero-subheader">Hero 2</h1>
+          <p className="hero-body">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            laboriosam nemo odit aperiam perspiciatis quasi.
+          </p>
+        </div>
+        <div className="hero-img">Image</div>
       </div>
-      <div className="hero-img">Image</div>
-    </div>
+    </Wrapper>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/NavbarWrapper';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="nav-center">
-        <span className="logo">ZCMI</span>
-        <div className="nav-links">
-          <NavLink to="/" className="nav-link">
-            Home
-          </NavLink>
-          <NavLink to="/technology" className="nav-link">
-            Technology
-          </NavLink>
-          <NavLink to="/about" className="nav-link">
-            About
-          </NavLink>
-          <NavLink to="/contact" className="nav-link">
-            Contact
-          </NavLink>
+    <Wrapper>
+      <nav>
+        <div className="nav-center">
+          <span className="logo">ZCMI</span>
+          <div className="nav-links">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/technology" className="nav-link">
+              Technology
+            </NavLink>
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+            <NavLink to="/contact" className="nav-link">
+              Contact
+            </NavLink>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </Wrapper>
   );
 };
 
