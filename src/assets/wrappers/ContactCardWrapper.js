2e1ca43a4c359acf5ccd7730efa777457985be79
Alex Cottam <alexcottam12@gmail.com>
2024-09-12 07:41:14 -0600

    add wrapper to ContactCard

---
+++ src/assets/wrappers/ContactCardWrapper.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  background-color: var(--white);
+
+  .contact-center {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 400px;
+  }
+
+  .contact-header {
+  }
+
+  .contact-btn {
+  }
+`;
+
+export default Wrapper;
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/ContactCardWrapper';
 
 const ContactCard = () => {
   return (
-    <div className="contact-center">
-      <h1 className="contact-header">Want to get in touch?</h1>
-      <button className="contact-btn">Contact Us</button>
-    </div>
+    <Wrapper>
+      <div className="contact-center">
+        <h1 className="contact-header">Want to get in touch?</h1>
+        <button className="contact-btn btn">Contact Us</button>
+      </div>
+    </Wrapper>
   );
 };
 
